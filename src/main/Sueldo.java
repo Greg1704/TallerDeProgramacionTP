4a087648c26d9a1db99b28f04b702e1f50ebe1d3
Greg1704 <70973430+Greg1704@users.noreply.github.com>
2022-10-02 20:58:21 -0300

    Clases generadas
    Falta lo de promocion

---
+++ src/main/Sueldo.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Sueldo {
+	private double basico;
+	private double porcentaje;
+	
+	
+	public Sueldo(double basico, double porcentaje) {
+		super();
+		this.basico = basico;
+		this.porcentaje = porcentaje;
+	}
+	
+	
+	public double getBasico() {
+		return basico;
+	}
+	public void setBasico(double basico) {
+		this.basico = basico;
+	}
+	public double getPorcentaje() {
+		return porcentaje;
+	}
+	public void setPorcentaje(double porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+	
+	
+}
