7226739a53a65850c5c5bfdda0f87d03444defb7
Greg1704 <70973430+Greg1704@users.noreply.github.com>
2022-11-07 16:47:50 -0300

    Base controlador hecha

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -3,6 +3,7 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import Ventana.IVista;
 import Ventana.VentanaAdministrador;
 import main.Sistema;
 
@@ -26,7 +27,73 @@ public class Controlador implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getActionCommand().equals(IVista.nuevoSistema)) {  //Ventana login
+			
+		}else if(e.getActionCommand().equals(IVista.guardaSistema)) {
+			
+		}else if(e.getActionCommand().equals(IVista.recuperaSistema)) {
+			
+		}else if(e.getActionCommand().equals(IVista.confirmaLoginUsuario)) {
+			
+		}else if(e.getActionCommand().equals(IVista.confirmaLogoutUsuario)) {
+			
+		}else if(e.getActionCommand().equals(IVista.confirmaEstadoMozoDia)) { //Ventana general
+			
+		}else if(e.getActionCommand().equals(IVista.asociaMozoAMesa)) {
+			
+		}else if(e.getActionCommand().equals(IVista.ocupaMesa)) {
+			
+		}else if(e.getActionCommand().equals(IVista.agregaPedido)) {
+			
+		}else if(e.getActionCommand().equals(IVista.cerrarComanda)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearOperario)) { //Ventana operarios y mesa
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarOperario)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarOperario)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearMesa)) {
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarMesa)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarMesa)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearPromPerm)) { //Ventana promociones
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarPromPerm)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarPromPerm)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearPromTemp)) {
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarPromTemp)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarPromTemp)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearMozo)) { //Ventana mozos
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarMozo)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarMozo)) {
+			
+		}else if(e.getActionCommand().equals(IVista.estadEmpleado)) {
+			
+		}else if(e.getActionCommand().equals(IVista.estadEmpleadoMayorVol)) {
+			
+		}else if(e.getActionCommand().equals(IVista.estadEmpleadoMenorVol)) {
+			
+		}else if(e.getActionCommand().equals(IVista.estadPromMesas)) {
+			
+		}else if(e.getActionCommand().equals(IVista.crearProducto)) { //Ventana productos
+			
+		}else if(e.getActionCommand().equals(IVista.eliminarProducto)) {
+			
+		}else if(e.getActionCommand().equals(IVista.modificarProducto)) {
+			
+		}else if(e.getActionCommand().equals(IVista.actualizarStockProducto)) {
+			
+		}
 		
 		
 	}
